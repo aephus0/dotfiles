ef11bc121cd5812cb6f714035b64c44baeebc852
aephus <aephus@duck.com>
2023-11-27 16:31:40 +0100

    nvim telescope-native-fzf fix

--- nvim/lua/aephus/plugins.lua
+++ nvim/lua/aephus/plugins.lua
@@ -37,17 +37,15 @@ require("lazy").setup({
 	"jose-elias-alvarez/null-ls.nvim",
 
 	"nvim-telescope/telescope.nvim",
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-	},
 
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", lazy = false },
 
 	"JoosepAlviste/nvim-ts-context-commentstring",
 
 	"lewis6991/gitsigns.nvim",
-	"kyazdani42/nvim-web-devicons",
+	"nvim-tree/nvim-web-devicons",
 	{
 		"nvim-tree/nvim-tree.lua",
 		version = "*",
